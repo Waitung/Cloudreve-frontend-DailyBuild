68c1b4fe7d13fb21e2bd80910ecc92076bef68c5
waitung <wai.tung@foxmail.com>
2021-01-16 09:26:56 +0000

    build 2021-01-16

---
+++ statics/precache-manifest.8a323bd2612fad1af49bf70b036cd6ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dff4b33556934971f9b85a50c2c0498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49be805423af0b31e9d1",
+    "url": "/static/css/6.81222343.chunk.css"
+  },
+  {
+    "revision": "3fcc61c24325da306915",
+    "url": "/static/js/10.0b0e2a01.chunk.js"
+  },
+  {
+    "revision": "b9f93350b07c808e65ac",
+    "url": "/static/js/11.ca753bb7.chunk.js"
+  },
+  {
+    "revision": "49be805423af0b31e9d1",
+    "url": "/static/js/6.f7900b7a.chunk.js"
+  },
+  {
+    "revision": "de62e0f99e1727a72db0",
+    "url": "/static/js/7.1e1d5029.chunk.js"
+  },
+  {
+    "revision": "7956ee776ce6fc1698f5",
+    "url": "/static/js/8.bec4f2b6.chunk.js"
+  },
+  {
+    "revision": "1fc095cf75a959a50122",
+    "url": "/static/js/9.e00bcaa2.chunk.js"
+  },
+  {
+    "revision": "7777d8f3f94e30d8d86b",
+    "url": "/static/js/codeEditor.96a79b3b.chunk.js"
+  },
+  {
+    "revision": "07afb3e549c8a949436d",
+    "url": "/static/js/main.9b49a3c7.chunk.js"
+  },
+  {
+    "revision": "a65d5ec7739e9fae553c",
+    "url": "/static/js/runtime-main.acd67ead.js"
+  }
+]);
